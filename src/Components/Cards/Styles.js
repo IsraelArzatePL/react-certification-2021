162ad96e8e0fd8501c451f73baa4d140e8bf8d67
IsraelArzatePL <israel.arzate@parklink.co>
2021-07-20 09:18:14 -0600

    Added CSS Grid

--- src/Components/Cards/Styles.js
+++ src/Components/Cards/Styles.js
@@ -2,20 +2,16 @@ import styled from 'styled-components'
 
 export const StyledCardsContainer = styled.div`
     width: 100%;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 4fr));
+    gap: 16px;
 `
 
 export const StyledCard = styled.div`
-    flex: 0 1 calc(25% - 12px);
+    display: inline-block;
     background-color: #fff;
     border-radius: 4px;
-    margin-top: 16px;
     overflow: hidden;
-    &:nth-of-type(-n+4) {
-        margin-top: 0;
-    }
     header {
         width: 100%;
         height: 300px;
